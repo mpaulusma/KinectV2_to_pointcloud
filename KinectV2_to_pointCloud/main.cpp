960e48fde1d0bd3abc520d482325a56b9543798f
Mick Paulusma <mick.paulusma@gmail.com>
2025-02-26 09:40:46 +0000

    Initial Commit

---
+++ KinectV2_to_pointCloud/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  KinectV2_to_pointCloud
+//
+//  Created by Mick Paulusma on 26/02/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
